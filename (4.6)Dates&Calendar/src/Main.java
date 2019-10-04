1903ccf3e0d745aa790f0c99a002b3ed4b1948b6
S0rago <linehalt10909@yandex.ru>
2019-10-04 18:51:35 +0300

    Добваил 4.6(дни на русском)

---
+++ (4.6)Dates&Calendar/src/Main.java
@@ -0,0 +1,17 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1999, Calendar.NOVEMBER, 1);
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i + " - " + dateFormat.format(calendar.getTime()) + " - " + calendar.getDisplayName(Calendar.DAY_OF_WEEK, 1, new Locale("Ru")));
+            calendar.add(Calendar.YEAR, 1);
+        }
+    }
+}
