cc00482e95f4b00fef1a37e80d8ac12dd3301d76
S0rago <linehalt10909@yandex.ru>
2019-10-07 11:59:38 +0300

    4.6 (Исправлено)

--- (4.6)Dates&Calendar/src/Main.java
+++ (4.6)Dates&Calendar/src/Main.java
@@ -1,6 +1,7 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class Main {
@@ -9,7 +10,7 @@ public class Main {
         DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         Calendar calendar = Calendar.getInstance();
         calendar.set(1999, Calendar.NOVEMBER, 1);
-        for (int i = 0; i <= 20; i++) {
+        for (int i = 0; calendar.getTime().getTime() <= System.currentTimeMillis(); i++) {
             System.out.println(i + " - " + dateFormat.format(calendar.getTime()) + " - " + calendar.getDisplayName(Calendar.DAY_OF_WEEK, 1, new Locale("Ru")));
             calendar.add(Calendar.YEAR, 1);
         }
