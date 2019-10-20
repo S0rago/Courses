0604be3f221a316ff4f7f59179d7d9a3fdb71ae8
S0rago <linehalt10909@yandex.ru>
2019-10-20 17:40:28 +0300

    Добавлено 7.2

--- (7.1)LambdaExpressions/src/Main.java
+++ (7.1)LambdaExpressions/src/Main.java
@@ -1,5 +1,6 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -13,17 +14,25 @@ public class Main
     public static void main(String[] args)
     {
         ArrayList<Employee> staff = loadStaffFromFile();
+        Calendar start = Calendar.getInstance();
+        start.set(2016, Calendar.DECEMBER, 31);
 
+        Calendar end = Calendar.getInstance();
+        end.set(2017, Calendar.DECEMBER, 31);
 
+        staff.stream()
+                .filter(e -> e.getWorkStart().after(start.getTime()) && e.getWorkStart().before(end.getTime()))
+                .max(Comparator.comparing(Employee::getSalary))
+                .ifPresent(System.out::println);
 //        staff.sort((s1, s2) -> {
 //            int salaryCompare = s1.getSalary().compareTo(s2.getSalary());
 //            //Если зарплаты равны, сравниваем имена
 //            return (salaryCompare == 0) ? s1.getName().compareTo(s2.getName()) : salaryCompare;
 //        });
-        staff.sort(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName));
-        for (Employee emp : staff) {
-            System.out.println(emp);
-        }
+//        staff.sort(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName));
+//        for (Employee emp : staff) {
+//            System.out.println(emp);
+//        }
     }
 
     private static ArrayList<Employee> loadStaffFromFile()
