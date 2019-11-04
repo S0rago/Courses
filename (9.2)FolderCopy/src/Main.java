4896fd0d0a16b30498eed5391fff5c867fab0c71
S0rago <linehalt10909@yandex.ru>
2019-11-04 14:39:18 +0300

    Добавлено 9.2

--- (9.2)FolderCopy/src/Main.java
+++ (9.2)FolderCopy/src/Main.java
@@ -1,8 +1,8 @@
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -20,6 +20,7 @@ public class Main {
                 File sourceFolder = new File(sourcePath);
                 File destinationFolder = new File(destinationPath);
                 copyFolder(sourceFolder, destinationFolder);
+                System.out.println("Done");
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
@@ -27,16 +28,17 @@ public class Main {
     }
 
     public static void copyFolder(File source, File dest) throws Exception {
-        for (File file : source.listFiles()) {
-            if (file.isDirectory())
-                copyFolder(file, dest);
-            else
-                Files.copy(file.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        if (source.isDirectory()) {
+            if (!dest.exists()) dest.mkdir();
+            String[] subSrc = source.list();
+            for (String file : subSrc) {
+                File subSrcFile = new File(source, file);
+                File destFile = new File(dest, file);
+                copyFolder(subSrcFile, destFile);
+            }
+        } else {
+            Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Copied: " + dest);
         }
-        System.out.println("Done");
-    }
-
-    public static boolean findInDest(File toFind, File dest) {
-        return true;
     }
 }
