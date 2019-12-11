9bac6dcefee992a9e93852c49a1e75a1bc67538a
S0rago <linehalt10909@yandex.ru>
2019-12-11 23:51:38 +0300

    Добавлено 9.5

--- (9.5)HtmlToJson/src/main/java/Main.java
+++ (9.5)HtmlToJson/src/main/java/Main.java
@@ -1,22 +1,16 @@
-import javafx.util.Pair;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
-    static Document doc;
+    private static Document doc;
     static {
         try {
             doc = Jsoup.connect("https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0")
@@ -26,55 +20,71 @@ public class Main {
             e.printStackTrace();
         }
     }
-    static HashMap<String,String> lines = new HashMap<>();
+    private static TreeMap<String,String> lines = new TreeMap<>();
+    private static List<String> stationsOnLine = new ArrayList<>();
+    private static JSONObject mainJsonObject = new JSONObject();
+    private static JSONObject lineObj = new JSONObject();
+
 
     public static void main(String[] args) {
         Elements tables = doc.select("table.sortable");
         for (Element tab : tables) {
             Elements rows = tab.select("tbody tr");
-            rows.remove(0);
             for (Element rw : rows) {
                 Elements tds = rw.select("td");
-                if (!tds.text().isBlank())
-                    parseRow(tds);
+                if (!tds.text().isBlank()) parseRow(tds);
             }
         }
-
-        lines.keySet().forEach(k -> System.out.println(k + " - " + lines.get(k)));
-        System.out.println(lines.size());
-
-        addToJSON();
+        mainJsonObject.put("stations", lineObj);
+        addLinesToJSON();
+        writeToJSON(mainJsonObject);
     }
 
-    public static void parseRow(Elements els) {
-        Pair<String, String> line = new Pair<>(els.get(0).select("span").first().text(), els.get(0).select("span").attr("title"));
-        lines.put(line.getKey(), line.getValue());
+    private static void parseRow(Elements els) {
+        String lineNum = els.get(0).select("span").first().text();
+        String lineName = els.get(0).select("span").attr("title");
         String station = els.get(1).select("a[href]").get(0).text();
         String cons = els.get(3).text();
-        System.out.println( line + " - " + station + " - " + cons);
+
+        if (lines.isEmpty()) {
+            lines.put(lineNum, lineName);
+            stationsOnLine.add(station);
+        }
+        else if (!lines.containsKey(lineNum)) {
+            addStationsToJSON(lines.lastKey());
+            lines.put(lineNum, lineName);
+            stationsOnLine.clear();
+        }
+        stationsOnLine.add(station);
+        System.out.println(lineNum + " - " + lineName + " - " + station + " - " + cons);
     }
 
-    public static void addToJSON() {
-        TreeMap<String, String> tree = new TreeMap<>(lines);
-        JSONObject jsonObject = new JSONObject();
-        String mainJsonObject = JSONValue.toJSONString(tree);
+    private static void addStationsToJSON(String lineNum) {
         JSONArray jsonArray = new JSONArray();
+        jsonArray.addAll(stationsOnLine);
+        lineObj.put(lineNum, jsonArray);
+    }
 
-//        lines.keySet().forEach(k -> {
-//                jsonObject.put("number", k);
-//                jsonObject.put("name", lines.get(k));
-//                jsonArray.add(jsonObject);
-//        });
-//
-//        mainJsonObject.put("lines", jsonArray);
+    private static void addLinesToJSON() {
+        JSONArray jsonArray = new JSONArray();
+        lines.forEach((key, value) -> {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("number", key);
+            jsonObject.put("name", value);
+            jsonArray.add(jsonObject);
+        });
+        mainJsonObject.put("lines", jsonArray);
+    }
+
+    private static void writeToJSON(JSONObject jObj) {
         try (FileWriter file = new FileWriter("src/main/test.json")) {
-            file.write(mainJsonObject);
+            file.write(jObj.toJSONString());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.print(mainJsonObject);
     }
 
-    public static void addCon() {}
+    private static void addCon() {/*
+    TODO Find a way to parse connections (go to con link and get name?)
+    */}
 }
