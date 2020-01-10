d0359f0aa307cc256827a3645c3bfd25eb7e2f70
S0rago <linehalt10909@yandex.ru>
2020-01-10 19:45:48 +0300

    Добавлено 10.2

---
+++ (10.2) Hibernate/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+
+        for (int i = 1;; i++) {
+            Student student = session.get(Student.class, i);
+            if(student == null) break;
+            System.out.println(student.toString());
+        }
+        sessionFactory.close();
+    }
+}
